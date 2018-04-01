249dd368d156bb326d364900dc8471a483da8b21
Jess Tucker <jess.tucker@gmail.com>
2018-04-01 11:17:49 -0700

    Adding troposphere script to generate a Jenkins instance

---
+++ jenkins-cf-template.py
@@ -0,0 +1,136 @@
+from troposphere.iam import InstanceProfile, PolicyType as IAMPolicy, Role
+from awacs.aws import Action, Allow, Policy, Principal, Statement
+from awacs.sts im port AssumeRole
+from troposphere import Base64, FindInMap, GetAtt
+from troposphere import Parameter, Output, Ref, Template, Join
+import troposphere.ec2 as ec2
+from ipaddress import ip_network
+from ipify import get_ip
+
+ApplicationName = "jenkins"
+ApplicationPort = "8080"
+
+GithubAccount = "jesstucker"
+GithubAnsibleURL = f'https://github.com/{GithubAccount}/ansible'
+AnsiblePullCmd = \
+    f'/user/local/bin/ansible-pull -U {GithubAnsibleURL} {ApplicationName}.yml'
+PublicCidrIp = str(ip_network(get_ip()))
+
+template = Template()
+
+keyname_param = template.add_parameter(Parameter(
+    "KeyName",
+    Description="Name of an existing EC2 KeyPair to enable SSH "
+                "access to the instance",
+    Type="AWS::EC2::KeyPair::KeyName",
+))
+
+template.add_mapping('RegionMap', {
+    "us-east-1": {"AMI": "ami-7f418316"},
+    "us-west-1": {"AMI": "ami-951945d0"},
+    "us-west-2": {"AMI": "ami-16fd7026"},
+    "eu-west-1": {"AMI": "ami-24506250"},
+    "sa-east-1": {"AMI": "ami-3e3be423"},
+    "ap-southeast-1": {"AMI": "ami-74dda626"},
+    "ap-northeast-1": {"AMI": "ami-dcfa4edd"}
+})
+
+template.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp=PublicCidrIp,
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "yum install --enablerepo=epel -y git",
+    "pip install ansible",
+    AnsiblePullCmd,
+    f'echo "*/10 * * * * {AnsiblePullCmd}" > /etc/cron.d/ansible-pull'
+]))
+
+template.add_resource(Role(
+    "Role",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["ec2.amazonaws.com"])
+            )
+        ]
+    )
+))
+
+template.add_resource(InstanceProfile(
+    "InstanceProfile",
+    Path="/",
+    Roles=[Ref("Role")]
+))
+
+
+ec2_instance = template.add_resource(ec2.Instance(
+    "Ec2Instance",
+    ImageId="ami-d874e0a0",
+    InstanceType="t1.micro",
+    KeyName=Ref(keyname_param),
+    SecurityGroups=[Ref("SecurityGroup")],
+    UserData=ud,
+    IamInstanceProfile=Ref("InstanceProfile"),
+))
+
+template.add_output([
+    Output(
+        "InstanceId",
+        Description="InstanceId of the newly created EC2 instance",
+        Value=Ref(ec2_instance),
+    ),
+    Output(
+        "AZ",
+        Description="Availability Zone of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "AvailabilityZone"),
+    ),
+    Output(
+        "PublicIP",
+        Description="Public IP address of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "PublicIp"),
+    ),
+    Output(
+        "PrivateIP",
+        Description="Private IP address of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "PrivateIp"),
+    ),
+    Output(
+        "PublicDNS",
+        Description="Public DNSName of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "PublicDnsName"),
+    ),
+    Output(
+        "PrivateDNS",
+        Description="Private DNSName of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "PrivateDnsName"),
+    ),
+    Output(
+        "WebUrl",
+        Description="Application endpoint",
+        Value=Join("", [
+            "http://", GetAtt(ec2_instance, "PublicDnsName"),
+            ":", ApplicationPort
+        ]),
+    )
+])
+
+print(template.to_json())
